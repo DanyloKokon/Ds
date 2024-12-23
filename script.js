cf36bc53aad1d72d6d7c4ff8f858c429897205d5
DanyloKokon <149526893+DanyloKokon@users.noreply.github.com>
2024-12-23 12:18:53 +0100

    ffffff

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById('btnAbout');
+    button.addEventListener('click', () => {
+        alert('Server made with love. WebSite made with GitHub, By: Nerv');
+    });
+});
